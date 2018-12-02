60b2c5c22989f819e1ea8dae0e695f4a02e3219f
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-12-02 13:15:15 +0100

    Rename input variable

--- day-02/index.js
+++ day-02/index.js
@@ -3,8 +3,8 @@ const { readFileSync } = require('fs');
 
 const { part1, part2 } = require('./solution');
 
-const i = readFileSync(resolve(__dirname, './input.txt'), 'utf8');
+const input = readFileSync(resolve(__dirname, './input.txt'), 'utf8');
 
 console.log('Day 2');
-console.log(`Part 1: ${part1(i)}`);
-console.log(`Part 2: ${part2(i)}`);
+console.log(`Part 1: ${part1(input)}`);
+console.log(`Part 2: ${part2(input)}`);
