6d96f628db84cd41ce07dd019a63e48e20aabc6b
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-12-03 22:01:49 +0100

    Update README

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/jonasstenberg/advent-of-code-2018/master/LICENSE)
 
 My take on [Advent of Code 2018](https://adventofcode.com/2018) using Node JS.
+
+## Install
+
+Install dependencies:
+
+```sh
+npm install
+```
+
+## Test
+
+Run all tests:
+
+```sh
+npm test
+```
+
+Run specific test:
+
+```sh
+npm test day-01/index.test.js
+```
--- day-01/index.js
+++ day-01/index.js
@@ -3,15 +3,12 @@
 const part1 = input => input.split('\n').reduce((a, c) => a + Number(c), 0);
 
 const part2 = (input) => {
-  const arr = input.split('\n').map(Number);
   const found = { 0: true };
   let sum = 0;
 
   while (true) {
-    for (const a of arr) {
-      if (a === 0) continue;
-
-      sum += a;
+    for (const line of input.split('\n').filter(l => l !== '').map(Number)) {
+      sum += line;
 
       if (found[sum]) {
         return sum;
